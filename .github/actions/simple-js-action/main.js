3970e445d815d7eff25945d0ed8d996b9355bc30
michal_urbanski <michal_urbanski@windowslive.com>
2024-11-04 19:50:02 +0100

    custom js action implementation

---
+++ .github/actions/simple-js-action/main.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const exec = require('@actions/exec')
+
+function run() {
+  core.notice('This is how you print something to step output');
+
+  const input = core.getInput('custom-name');
+
+  core.exec('ls') // A way to execute any installed command on the runner.
+
+  core.notice(input);
+  core.setOutput('js-output', 'Custom js action output value');
+}
+
+run();
