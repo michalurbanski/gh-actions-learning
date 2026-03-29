287c78fc9f3f3ac6c458f78653552ff02d71a902
michal urbanski <michal_urbanski@windowslive.com>
2026-03-29 22:07:22 +0200

    checking

--- .github/actions/bun-composite-action/index.ts
+++ .github/actions/bun-composite-action/index.ts
@@ -2,10 +2,11 @@ import * as core from '@actions/core';
 
 async function run(): Promise<void> {
   try {
-    const nameToGreet = core.getInput('who_to_greet');
+    const nameToGreet = core.getInput('INPUT_WHO_TO_GREET');
     core.info(`Hello ${nameToGreet}`);
 
-    const time = new Date().toTimeString();
+    // const time = new Date().toTimeString();
+    const time = "outputvalue";
     core.setOutput('time', time);
   }
   catch (error) {
