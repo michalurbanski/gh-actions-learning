00646a271f3db8eb10043953137447203ebe75e6
michal urbanski <michal_urbanski@windowslive.com>
2026-03-29 22:15:50 +0200

    additional comments for explanations

--- .github/actions/bun-composite-action/index.ts
+++ .github/actions/bun-composite-action/index.ts
@@ -2,10 +2,14 @@ import * as core from '@actions/core';
 
 async function run(): Promise<void> {
   try {
+    // !!! Notice, that INPUT_WHO_TO_GREET is passed, but here there's no INPUT_ and it works correctly. Has to be like this.
     const nameToGreet = core.getInput('WHO_TO_GREET');
     core.info(`Hello ${nameToGreet}`);
 
+    // returning date causes some parsing error for the output
     // const time = new Date().toTimeString();
+
+    // so, returning a simple value instead
     const time = "outputvalue";
     core.setOutput('time', time);
   }
